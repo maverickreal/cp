fe820454fcee3993ee3bdd14cde523845efca7f6
Aditya Saxena <adity09@outlook.com>
2022-09-28 11:32:23 +0530

    clean

--- main.cpp
+++ main.cpp
@@ -5,87 +5,83 @@
 #include "stack"
 #include "unordered_map"
 #include "unordered_set"
+#include "set"
+#include "map"
 #include "math.h"
 #include "algorithm"
 #include "queue"
 #include "functional"
 #include "numeric"
-#include "segmentTree.h"
+#include "iomanip"
+#include "bitset"
 
 using namespace std;
 
-template<typename a, typename b, typename c>
-class tri {
-public:
-    a x;
-    b y;
-    c z;
-    tri(a x, b y, c z) {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
-    tri() {
-        this->x = *(new a);
-        this->y = *(new b);
-        this->z = *(new c);
-    }
-};
-
 typedef long long ll;
-typedef long double ld;
-typedef vector<ll> vi;
-#define endl '\n'
-static const ll mod = 1e9;
+typedef pair<ll, ll> pi;
+#define vi(x) vector<x>
+#define pb push_back
+const ll mod = 1e9 + 7;
+const char nl = '\n';
 
-vector<bool>primes;
+bitset<0> primes(0);
 
-void setPrimes(ll n);
+void setPrimes();
 
-vector<vector<ll>>getChildren(const vector<ll>& v);
+bool isPrime(ll n);
+
+vi(vi(ll)) getChildren(vi(ll)& v);
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-    ll t = 1;
-    //cin >> t;
-    while (t--) {
-        ll sz;
-        cin >> sz;
-        vector<ll>v(sz);
-        for (ll& i : v)
-            cin >> i;
-        st tree(v);
-        ll q;
-        cin >> q;
-        while (q--) {
-            ll a, b;
-            char ch;
-            cin >> a >> b >> ch;
-            if (ch == 'u')
-                tree.updateValue(a, b);
-            else if (ch == 'g')
-                cout << tree.getSum(a, b) << endl;
-        }
+    cout << fixed << setprecision(10);
+    ll tc = 1;
+    cin >> tc;
+    while (tc--) {
     }
     return 0;
 }
 
-void setPrimes(ll n) {
-    vector<bool>v(n + 1, true);
-    v[0] = v[1] = false;
+void setPrimes() {
+    ll n = primes.size();
+    primes[0] = primes[1] = 0;
     for (ll i = 2;i * i <= n;i++) {
-        if (v[i]) {
+        if (primes[i]) {
             for (ll j = i;j * i <= n;++j)
-                v[j * i] = false;
+                primes[j * i] = 0;
         }
     }
-    primes = v;
 }
 
-vector<vector<ll>>getChildren(const vector<ll>& v) {
+bool isPrime(ll n) {
+    if (n == 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    for (ll i = 3; i * i <= n; i += 2)
+        if (n % i == 0) return false;
+    return true;
+}
+
+vi(ll) getFactors(ll& n) {
+    if (n < 0)
+        return {};
+    if (n == 0)
+        return { 0 };
+    vi(ll) res { 1 };
+    for (ll i = 2;i * i <= n;i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if ((n / i) != i)
+                res.push_back(n / i);
+        }
+    }
+    return res;
+}
+
+vi(vi(ll)) getChildren(vi(ll)& v) {
     ll n = v.size();
-    vector<vector<ll>>res(n);
+    vi(vi(ll)) res(n);
     for (ll i = 0;i < n;++i) {
         ll node = i + 1, par = v[i];
         if (par == -1)continue;
@@ -93,3 +89,33 @@ vector<vector<ll>>getChildren(const vector<ll>& v) {
     }
     return res;
 }
+
+class dsu{
+public:
+    vi(ll)par, size;
+    dsu(n){
+        par.assign(n, 0), size(n, 1);
+        for(ll i=0;i<n;++i){
+            par[i]=i;
+        }
+    }
+    ll find(ll n){
+        if(par[n]==n){
+            return n;
+        }
+        ll p=find(par[n]);
+        par[n]=p;
+        return p;
+    }
+    void union(ll a, ll b){
+        a=find(a), b=find(b);
+        if(a==b){
+            return;
+        }
+        if(size[a]<size[b]){
+            a^=b, b^=a, a^=b;
+        }
+        par[b]=a;
+        size[a]+=size[b];
+    }
+};
