054c9041a3bf7198215b4296a8eb7284271fb71b
Aditya Saxena <adity09@outlook.com>
2023-10-24 20:34:17 +0530

    reverted extendedeucledian back to recursion

--- math/mod.cpp
+++ math/mod.cpp
@@ -16,12 +16,12 @@ ll modBinExp(ll x, ll y, ll m){
 
 vi(ll) extendedEucledian(ll a, ll b) {
     vi(ll) x = {1, 0, a}, y = {0, 1, b};
-	while (y[2] > 0) {
-		ll k = x[2] / y[2];
-		for (ll i = 0; i < 3; ++i){
-            x[i] -= k * y[i];
+    while (y[2] > 0) {
+        ll k = x[2] / y[2];
+        for (ll i = 0; i < 3; ++i){
+             x[i] -= k * y[i];
         }
-		swap(x, y);
-	}
-	return x;
+        swap(x, y);
+    }
+    return x;
 }
